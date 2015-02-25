c631374071579ea7b17e6e97ad8d902ef9985494
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-02-26 00:00:32 +0100

    Fix #10 and refactor in anticipation of #3.
    The fix is rename of BGD->MiniSGD.
    The refactor is to abstract the minibatch-handling away.

--- nnet_opt.py
+++ nnet_opt.py
@@ -7,9 +7,9 @@ import numpy as np
 from lbtoolbox.util import batched_padded
 
 
-class BatchGradDescent(object):
+class MinibatchOptimizer(object):
 
-    def __init__(self, model, nllclip=(1e-15, 1-1e-15)):
+    def __init__(self, model, nllclip):
         # Keep in mind for later.
         self.model = model
         self.nllclip = nllclip
@@ -21,30 +21,37 @@ class BatchGradDescent(object):
         self.fn_nll = th.function(inputs=[], outputs=model.nll)
 
         # And one for computing all of the above jointly, much faster.
+        # Except cost, which is practically useless for evaluation.
         self.fn_eval = th.function(inputs=[], outputs=[model.nll, model.errors, model.pred_proba])
 
-        # And finally the training part.
-        # TODO: Could still output cost, as it's computed anyways...
-        # Fuck transfercost of 1 number?
+        # The training function is what changes throughout different methods,
+        # so the specific subclasses will need to compile that.
+        # They all share the fact of having a learning-rate, though.
         self.sh_learningrate = T.scalar('lrate')
-        g = T.grad(cost=model.cost, wrt=model.params)
-        self.fn_train = th.function(
-            inputs=[self.sh_learningrate],
-            outputs=[model.cost, model.nll],
-            updates=[(p, p - self.sh_learningrate * gp) for p, gp in zip(model.params, g)]
-        )
 
 
     def fit_epoch(self, X, y, learning_rate, aug=None):
         costs, nlls = [], []
+        # Go through the training in minibatches.
+        # As long as there's https://github.com/Theano/Theano/issues/2464
+        # we need to pad the last batch.
         for _, bx, by in batched_padded(self.model.batchsize, X, y):
+            # Potentially generate a new augmentation on-the-fly.
             if aug:
                 bx = aug.augbatch_train(bx)
+
+            # Upload the minibatch to the GPU.
             self.model.sh_x.set_value(th.sandbox.cuda.CudaNdarray(bx))
             self.model.sh_y.set_value(by)
+
+            # Do the forward *and* backward pass.
             cost, nll = self.fn_train(learning_rate)
+
+            # Collect stats over the batches, so we can average.
             costs.append(cost)
             nlls.append(nll)
+
+        # Average the stats over the batches.
         return sum(costs)/len(costs), sum(nlls)/len(nlls)
 
 
@@ -55,9 +62,12 @@ class BatchGradDescent(object):
         # As long as there's https://github.com/Theano/Theano/issues/2464
         # we need to pad the last batch.
         for bs, bx, by in batched_padded(self.model.batchsize, X, y):
+            # For scoring, augmentation or not makes a big difference:
             if aug:
-                # In the case of augmentation, average all outputs of the
-                # augmenters. ("Return of the Devil in the Details", Chatfield, Simonyan, Vevaldi & Zisserman)
+                # With augmentation, the model will be evaluated on potentially
+                # many augmented versions of each batch and we need to average
+                # the output class-probabilities of all those runs.
+                # See "Return of the Devil in the Details" for details.
                 ppreds = []
                 for bx_aug in aug.augbatch_pred(bx, fast):
                     self.model.sh_x.set_value(th.sandbox.cuda.CudaNdarray(bx_aug))
@@ -65,13 +75,33 @@ class BatchGradDescent(object):
                     # padded part of the last batch.
                     ppreds.append(self.fn_pred_proba()[:bs])
                 p_y_given_x = sum(ppreds)/len(ppreds)
+                # TODO: Most of this actually belongs into the costlayer's
+                #       class, since it's even very classification-specific!
+                # Since we have the NLL on the CPU, we'll have to compute it here.
                 nll = -np.mean(np.log(np.clip(p_y_given_x[np.arange(bs), by[:bs]], *self.nllclip)))
                 err = np.sum(np.argmax(p_y_given_x[:bs], axis=1) != by[:bs])
             else:
+                # While without augmentation, it's the straightforward thing.
                 self.model.sh_x.set_value(th.sandbox.cuda.CudaNdarray(bx))
                 self.model.sh_y.set_value(by)
                 nll, err, pred = self.fn_eval()
+            # Again, collect over batches...
             nlls.append(nll)
             errs += err
+        # ...so we can return average nll over batches, and total errors.
         return sum(nlls)/len(nlls), errs
 
+
+class MiniSGD(MinibatchOptimizer):
+
+    def __init__(self, model, nllclip=(1e-15, 1-1e-15)):
+        super(MiniSGD, self).__init__(model, nllclip)
+
+        # For SGD, training is quite simple:
+        # p_e+1 = p_e - lr * grad(p_e)
+        g = T.grad(cost=model.cost, wrt=model.params)
+        self.fn_train = th.function(
+            inputs=[self.sh_learningrate],
+            outputs=[model.cost, model.nll],
+            updates=[(p, p - self.sh_learningrate * gp) for p, gp in zip(model.params, g)]
+        )

6c25a22e734a373a833fe8b35f8736e31cc5d51b
lucasb-eyer <lucasb.eyer.be@gmail.com>
2015-07-23 15:17:35 +0200

    Turn into an installable package.

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='lbtoolbox',
+      version='0.01',
+      description="Lucas' toolbox of misc. things he uses/does frequently.",
+      author='Lucas Beyer',
+      author_email='lucasb.eyer.be@gmail.com',
+      url='http://github.com/lucasb-eyer/lbtoolbox',
+      packages=find_packages())

b374df8292aff0c036b531fb45c13bc76e69f4d8
lucasb-eyer <lucasb.eyer.be@gmail.com>
2016-01-29 13:26:39 +0100

    `check_random_state` accepts `True` now.

--- lbtoolbox/util.py
+++ lbtoolbox/util.py
@@ -194,12 +194,12 @@ def check_random_state(seed):
     """
     Turn `seed` into a `np.random.RandomState` instance.
 
-    - If `seed` is `None`, return the `RandomState` singleton used by `np.random`.
+    - If `seed` is `None` or `True`, return the `RandomState` singleton used by `np.random`.
     - If `seed` is an `int`, return a new `RandomState` instance seeded with `seed`.
     - If `seed` is already a `RandomState` instance, return it.
     - Otherwise raise `ValueError`.
     """
-    if seed is None or seed is _np.random:
+    if seed in (None, True, _np.random):
         return _np.random.mtrand._rand
 
     if isinstance(seed, (numbers.Integral, _np.integer)):

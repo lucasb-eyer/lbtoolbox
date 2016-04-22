3d7e5caf4566ed9b9755e259e19a7bdcc988c69e
lucasb-eyer <lucasb.eyer.be@gmail.com>
2016-04-22 14:09:25 +0200

    Add 3 standard measures to conf-mat eval:
    - jaccard iou
    - accuracy
    - mean precision

--- lbtoolbox/evaluation.py
+++ lbtoolbox/evaluation.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def fast_confusion_matrix(y_pred, y_gt, nlabels):
     """
     NOTE: This can certainly be extended to arbitrary labels without affecting
@@ -10,3 +12,17 @@ def fast_confusion_matrix(y_pred, y_gt, nlabels):
         preds, n = np.unique(y_pred[y_gt == gt], return_counts=True)
         cm[gt, preds] = n
     return cm
+
+
+def jaccard_iou(cm):
+    s = np.sum(cm, axis=0) + np.sum(cm, axis=1) - np.diag(cm)
+    return np.diag(cm)/s
+
+
+def accuracy(cm):
+    return cm.trace()/cm.sum()
+
+
+def mean_precision(cm):
+    rcm = cm / np.sum(cm, axis=1, keepdims=True)
+    return rcm.trace() / len(cm)

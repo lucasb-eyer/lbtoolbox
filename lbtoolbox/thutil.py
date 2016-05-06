2f61e2d31220423006dd0c173623f40e0a181537
lucasb-eyer <lucasb.eyer.be@gmail.com>
2016-05-06 10:21:42 +0200

    Add `hasnan` and `showstats` utils for theano models.

--- lbtoolbox/thutil.py
+++ lbtoolbox/thutil.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
+# coding: utf-8
 
 import theano as th
 import theano.tensor as T
 import numpy as np
 import os
 from zlib import crc32
+
+from .util import printnow
 
 
 def on_gpu(fn):
@@ -52,3 +55,33 @@ def load_model(model, fname, hashmodel=True):
 def count_params(model):
     params, _ = model.parameters()
     return sum(np.prod(p.get_value().shape) for p in params)
+
+
+def hasnan(model, params=True, grads=True):
+    nans = []
+    for p, g in zip(*model.parameters()):
+        # TODO: If this ever turns into a bottleneck, use Bottleneck! (py lib)
+        if params and np.isnan(np.sum(p.get_value())):
+            nans.append(p)
+        if grads and np.isnan(np.sum(g.get_value())):
+            nans.append(g)
+    return nans
+
+
+def showstats(net, param=True, grad=True):
+    params, grads = net.parameters()
+    maxlen = max(len(p.name) for p in params)
+    if param:
+        for p in params:
+            v = p.get_value()
+            fmt = "{name:{namew}} σ={std:.4f}"
+            kw = {'name': p.name + ':', 'namew': maxlen+1, 'std': np.std(v)}
+            if p.ndim == 2:
+                fmt += ', ρ={specrad:.4f}'
+                kw['specrad'] = max(abs(np.linalg.svd(v, compute_uv=False)))
+            printnow(fmt, **kw)
+
+    if grad:
+        for p, g in zip(params, grads):
+            v = g.get_value()
+            printnow("{:{namew}} ∈ [{: .5f},{: .5f}]", p.name, np.min(v), np.max(v), namew=maxlen)

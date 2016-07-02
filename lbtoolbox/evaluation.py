ba1ba35f2575cbe0f7029cfaaa9df3fcd169a04f
lucasb-eyer <lucasb.eyer.be@gmail.com>
2016-07-02 05:52:31 +0200

    Confusion matrix can add to existing one.
    This allows for batch-wise or even sample-wise building up the full
    confusion matrix if data is too large to fit memory.

--- lbtoolbox/evaluation.py
+++ lbtoolbox/evaluation.py
@@ -1,16 +1,24 @@
 import numpy as np
 
-def fast_confusion_matrix(y_pred, y_gt, nlabels):
+def fast_confusion_matrix(y_pred, y_gt, nlabels=None, cm=None):
     """
     NOTE: This can certainly be extended to arbitrary labels without affecting
           speed too much, I just didn't care yet.
     NOTE: This correctly ignores `-1` entries in `y_gt`, but assumes there are
           *only* valid labels in `y_pred`.
     """
-    cm = np.zeros((nlabels,nlabels), np.uint64)
+    assert cm is not None or nlabels is not None, "Need either pre-allocated `cm` or `nlabels`."
+
+    if cm is None:
+        cm = np.zeros((nlabels,nlabels), np.int64)  # Needs to be `int64` not `uint64` for `+=` below to work.
+    elif nlabels is None:
+        nlabels = cm.shape[0]
+    else:
+        assert cm.shape == (nlabels, nlabels), "Mismatch between `cm`'s shape and `nlabels`."
+
     for gt in range(nlabels):
         preds, n = np.unique(y_pred[y_gt == gt], return_counts=True)
-        cm[gt, preds] = n
+        cm[gt, preds] += n
     return cm
 
 
